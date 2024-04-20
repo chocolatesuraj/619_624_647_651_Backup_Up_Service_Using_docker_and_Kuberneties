4c8ade72c10ec718a59f456f45dca43ac817c0e3
Suvan <suvan362@gmail.com>
2024-04-20 13:35:52 +0530

    pvc and pv

--- run.ps1
+++ run.ps1
@@ -1,5 +1,6 @@
-minikube start
+minikube start --mount --mount-string="$(pwd)\backupfolder:/Mount"
 docker context use default
 minikube docker-env
 minikube docker-env | Invoke-Expression
-kubectl apply -f backuper.yaml
+docker build -t backuper .
+kubectl apply -f backuper.yaml -f pv.yaml -f pvc.yaml
