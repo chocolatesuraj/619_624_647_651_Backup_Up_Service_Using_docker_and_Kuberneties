e6ae02b40eb1b48e02ad13935165cca10c5772b9
MelonMusk24 <97746462+MelonMusk24@users.noreply.github.com>
2024-04-23 15:38:40 +0530

    secret run.ps1

--- run.ps1
+++ run.ps1
@@ -3,4 +3,5 @@ docker context use default
 minikube docker-env
 minikube docker-env | Invoke-Expression
 docker build -t backuper .
+kubectl create secret generic google-drive-credentials --from-file=token.json
 kubectl apply -f backuper.yaml -f pv.yaml -f pvc.yaml
